f069a97bd4e48c834109679d969c4fcfed23dd19
Graeme Bell <bell.graeme@gmail.com>
2017-04-04 14:46:47 +0100

    no tests

---
+++ specs/basket_spec.js
@@ -0,0 +1,16 @@
+var assert =  require('assert');
+var Basket =  require('../basket.js')
+
+
+describe('Basket Test', function(){
+
+  var basket;
+
+  beforeEach(function(){
+    basket = new Basket();
+  })
+
+  
+
+
+})
