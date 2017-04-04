e10e69232d2711a16a7ed6c0995e0070604d0b93
Graeme Bell <bell.graeme@gmail.com>
2017-04-04 14:54:28 +0100

    First Item Test passed

---
+++ item.js
@@ -0,0 +1,6 @@
+var Item = function(name){
+  this.name = name;
+}
+
+
+module.exports = Item;
--- specs/item_spec.js
+++ specs/item_spec.js
@@ -7,9 +7,13 @@ describe('Item Test', function(){
   var item;
 
   beforeEach(function(){
-    item = new Item();
+    item = new Item("Mars");
   })
 
+
+it('item should have name', function(){
+  assert.strictEqual("Mars", item.name)
+})
   
 
   
